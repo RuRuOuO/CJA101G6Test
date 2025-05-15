449a4941e67f36bccc3659b68dd43897c25eab5a
Vail Chen <vivi88605@gmail.com>
2025-05-15 11:25:04 +0800

    aaaaaaa

--- src/main/java/com/islevilla/weijing/test/TestClass.java
+++ src/main/java/com/islevilla/weijing/test/TestClass.java
@@ -2,6 +2,7 @@ package com.islevilla.weijing.test;
 
 public class TestClass {
 	public static void main(String[] args) {
-		System.out.println("good morning");		
+		System.out.println("good morning");	
+		
 	}
 }
