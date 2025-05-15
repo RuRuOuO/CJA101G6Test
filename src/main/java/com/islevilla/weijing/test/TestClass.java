468d977167667c9bcf18ded4f83f54799c232768
Vail Chen <vivi88605@gmail.com>
2025-05-15 11:27:13 +0800

    another commit

--- src/main/java/com/islevilla/weijing/test/TestClass.java
+++ src/main/java/com/islevilla/weijing/test/TestClass.java
@@ -3,6 +3,6 @@ package com.islevilla.weijing.test;
 public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("good morning");	
-		
+		System.out.println("AAAAAAAAAAAAAA");
 	}
 }
